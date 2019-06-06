a08ba2d668644262a06bbc7634b509f6865971bd
4landre <450755451@qq.com>
2019-06-06 16:27:10 +0800

    增加图标及颜色选项

--- main.js
+++ main.js
@@ -1,49 +1,12 @@
 var canvas = document.getElementById('canvas');//获取canvas
 //设置canvas全屏
 autosetwindow()
-
 window.onresize = function () {
     autosetwindow()
 }
-
 var context = canvas.getContext('2d');//获取二次元上下文
 
-var using = false
-var lastXY = { x: undefined, y: undefined }
-
-document.onmousedown = function (xy) { //点击鼠标api可获得相关参数
-    var x = xy.clientX
-    var y = xy.clientY
-    lastXY = { x: x, y: y }
-    using = true
-    if (enableEraser) {
-        context.clearRect(x - 5, y - 5, 10, 10);
-    }
-    else {
-        drawCircle(x, y, 1);
-    }
-
-}
-
-document.onmousemove = function (xy) {
-    var x = xy.clientX
-    var y = xy.clientY
-    if (using) {
-        if (enableEraser) {
-            context.clearRect(x - 5, y - 5, 10, 10)
-        } else {
-            var newXY = { x: x, y: y }
-            drawLine(lastXY.x, lastXY.y, newXY.x, newXY.y)
-            lastXY = newXY
-        }
-    }
-
-}
-
-
-document.onmouseup = function (xy) {
-    using = false
-}
+listenToUesr()
 
 //工具
 
@@ -56,18 +19,104 @@ function autosetwindow() {
     canvas.height = pageHeight
 }
 
+//监听鼠标
+function listenToUesr() {
+    var using = false
+    var lastXY = { x: undefined, y: undefined }
+    black.onclick = function () {
+        black.classList.add('active')
+        red.classList.remove('active')
+        blue.classList.remove('active')
+        context.fillStyle = 'black'
+        context.strokeStyle ='black'
+    }
+    red.onclick = function () {
+        black.classList.remove('active')
+        red.classList.add('active')
+        blue.classList.remove('active')
+        context.fillStyle = 'red'
+        context.strokeStyle ='red'
+    }
+    blue.onclick = function () {
+        black.classList.remove('active')
+        red.classList.remove('active')
+        blue.classList.add('active')
+        context.fillStyle = 'blue'
+        context.strokeStyle ='blue'
+    }
+
+    if (document.ontouchstart !== undefined) { //特性检测（用户是在什么客户端上）
+        canvas.ontouchstart = function (xy) { //用触摸屏画
+            var x = xy.touches[0].clientX
+            var y = xy.touches[0].clientY
+            lastXY = { x: x, y: y }
+            using = true
+            if (enableEraser) {
+                context.clearRect(x - 5, y - 5, 10, 10);
+            }
+            else {
+                drawCircle(x, y, 1);
+            }
+        }
+        canvas.ontouchmove = function (xy) {
+            var x = xy.touches[0].clientX
+            var y = xy.touches[0].clientY
+            if (using) {
+                if (enableEraser) {
+                    context.clearRect(x - 5, y - 5, 10, 10)
+                } else {
+                    var newXY = { x: x, y: y }
+                    drawLine(lastXY.x, lastXY.y, newXY.x, newXY.y)
+                    lastXY = newXY
+                }
+            }
+        }
+        canvas.ontouchend = function () {
+            using = false
+        }
+    } else {
+        document.onmousedown = function (xy) { //用鼠标画
+            var x = xy.clientX
+            var y = xy.clientY
+            lastXY = { x: x, y: y }
+            using = true
+            if (enableEraser) {
+                context.clearRect(x - 5, y - 5, 10, 10);
+            }
+            else {
+                drawCircle(x, y, 1);
+            }
+        }
+        document.onmousemove = function (xy) {
+            var x = xy.clientX
+            var y = xy.clientY
+            if (using) {
+                if (enableEraser) {
+                    context.clearRect(x - 5, y - 5, 10, 10)
+                } else {
+                    var newXY = { x: x, y: y }
+                    drawLine(lastXY.x, lastXY.y, newXY.x, newXY.y)
+                    lastXY = newXY
+                }
+            }
+
+        }
+        document.onmouseup = function (xy) {
+            using = false
+        }
+    }
+
+}
+
 //画圆
 function drawCircle(x, y, radious) {
     context.beginPath();
-    context.fillStyle = 'black';
     context.arc(x, y, 1, 0, Math.PI * 2);
-    context.fi
-    ll();
+    context.fill();
 }
 //2点连线
 function drawLine(x1, y1, x2, y2) {
     context.beginPath();
-    context.strokeStyle = 'black';
     context.moveTo(x1, y1);
     context.lineWidth = 1;
     context.lineTo(x2, y2);
@@ -78,9 +127,11 @@ function drawLine(x1, y1, x2, y2) {
 var enableEraser = false
 eraser.onclick = function () {
     enableEraser = true
-    action.className = "eraser"
+    eraser.classList.add('active')
+    pencil.classList.remove('active')
 }
-brush.onclick = function () {
+pencil.onclick = function () {
     enableEraser = false
-    action.className = "brush"
+    eraser.classList.remove('active')
+    pencil.classList.add('active')
 }
