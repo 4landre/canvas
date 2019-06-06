b31c8ce2d51b0b98d9c62e7228e81be20aa42801
4landre <450755451@qq.com>
2019-06-06 20:36:32 +0800

    增加重画，优化图标

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 var canvas = document.getElementById('canvas');//获取canvas
 //设置canvas全屏
-autosetwindow()
+setwindowsize()
 window.onresize = function () {
-    autosetwindow()
+    setwindowsize()
 }
-var context = canvas.getContext('2d');//获取二次元上下文
 
+var context = canvas.getContext('2d');//获取二次元上下文
+var lineWidth = 5
 listenToUesr()
 
 //工具
 
 //全屏化canvas
-function autosetwindow() {
+
+function setwindowsize() {
     var pageWidth = document.documentElement.clientWidth
     var pageHeight = document.documentElement.clientHeight
 
@@ -19,30 +21,45 @@ function autosetwindow() {
     canvas.height = pageHeight
 }
 
+
+
+
 //监听鼠标
 function listenToUesr() {
     var using = false
     var lastXY = { x: undefined, y: undefined }
-    black.onclick = function () {
+    black.onclick = function () { //选择颜色
         black.classList.add('active')
         red.classList.remove('active')
         blue.classList.remove('active')
         context.fillStyle = 'black'
-        context.strokeStyle ='black'
+        context.strokeStyle = 'black'
     }
     red.onclick = function () {
         black.classList.remove('active')
         red.classList.add('active')
         blue.classList.remove('active')
         context.fillStyle = 'red'
-        context.strokeStyle ='red'
+        context.strokeStyle = 'red'
     }
     blue.onclick = function () {
         black.classList.remove('active')
         red.classList.remove('active')
         blue.classList.add('active')
         context.fillStyle = 'blue'
-        context.strokeStyle ='blue'
+        context.strokeStyle = 'blue'
+    }
+
+
+    thin.onclick = function () {
+        lineWidth = 5
+        thin.classList.add('active')
+        thick.classList.remove('active')
+    }
+    thick.onclick = function () {
+        lineWidth = 10
+        thin.classList.remove('active')
+        thick.classList.add('active')
     }
 
     if (document.ontouchstart !== undefined) { //特性检测（用户是在什么客户端上）
@@ -66,6 +83,7 @@ function listenToUesr() {
                     context.clearRect(x - 5, y - 5, 10, 10)
                 } else {
                     var newXY = { x: x, y: y }
+                    drawCircle(x, y, 1);
                     drawLine(lastXY.x, lastXY.y, newXY.x, newXY.y)
                     lastXY = newXY
                 }
@@ -81,7 +99,7 @@ function listenToUesr() {
             lastXY = { x: x, y: y }
             using = true
             if (enableEraser) {
-                context.clearRect(x - 5, y - 5, 10, 10);
+                context.clearRect(x - 10, y - 10, 20, 20);
             }
             else {
                 drawCircle(x, y, 1);
@@ -95,6 +113,7 @@ function listenToUesr() {
                     context.clearRect(x - 5, y - 5, 10, 10)
                 } else {
                     var newXY = { x: x, y: y }
+                    drawCircle(x, y, 1);
                     drawLine(lastXY.x, lastXY.y, newXY.x, newXY.y)
                     lastXY = newXY
                 }
@@ -111,14 +130,14 @@ function listenToUesr() {
 //画圆
 function drawCircle(x, y, radious) {
     context.beginPath();
-    context.arc(x, y, 1, 0, Math.PI * 2);
+    context.arc(x, y, lineWidth * 0.5, 0, Math.PI * 2);
     context.fill();
 }
 //2点连线
 function drawLine(x1, y1, x2, y2) {
     context.beginPath();
     context.moveTo(x1, y1);
-    context.lineWidth = 1;
+    context.lineWidth = lineWidth;
     context.lineTo(x2, y2);
     context.stroke();
 }
@@ -134,4 +153,7 @@ pencil.onclick = function () {
     enableEraser = false
     eraser.classList.remove('active')
     pencil.classList.add('active')
+}
+clear.onclick = function () {
+    context.clearRect(0, 0, canvas.width, canvas.height)
 }
